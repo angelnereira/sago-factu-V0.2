7b8bfc1bdef46870da0c1a7c22319d77353d175a
angelnereira <angelnereira15@gmail.com>
2025-11-16 18:09:00 -0500

    chore: add logger alias for backward compatibility
    - Create @/lib/logger.ts that exports from @/lib/utils/logger
    - Allows imports from either @/lib/logger or @/lib/utils/logger
    - Prevents future build errors if logger is imported from alternative path

---
+++ lib/logger.ts
@@ -0,0 +1,8 @@
+/**
+ * Logger - Alias para @/lib/utils/logger
+ *
+ * Este archivo proporciona un alias para @/lib/utils/logger
+ * por compatibilidad con código que lo importa desde @/lib/logger
+ */
+
+export { logger, createLogger, loggers } from '@/lib/utils/logger';
